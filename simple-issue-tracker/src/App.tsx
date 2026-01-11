34e8a84f70a0bb5e3c5c469066f74c70834d83b3
Mihai <alexmihai.ciobanu@gmail.com>
2026-01-11 16:52:48 +0000

    Add empty QueryClient.

--- simple-issue-tracker/src/App.tsx
+++ simple-issue-tracker/src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create React Query client
+const queryClient = new QueryClient();
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +33,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 

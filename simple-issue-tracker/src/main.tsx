36a7b4fddca1b58269ba3f9a7f8c57e365a43ceb
Mihai <alexmihai.ciobanu@gmail.com>
2026-01-11 17:28:22 +0000

    Move React Query wrapper to main.tsx.

--- simple-issue-tracker/src/main.tsx
+++ simple-issue-tracker/src/main.tsx
@@ -4,11 +4,23 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create React Query client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 60000, refetchOnWindowFocus: false },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );

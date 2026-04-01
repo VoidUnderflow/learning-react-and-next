380ac718fce4408a24ad38a4ed8f95623d91d0e9
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-01 16:08:07 +0300

    Extract page layout rule.

--- pagination-experiments/src/pages/HomePage.tsx
+++ pagination-experiments/src/pages/HomePage.tsx
@@ -12,7 +12,7 @@ function HomePage() {
     if (location.pathname !== "/") return <Outlet />;
 
     return (
-        <div className="flex flex-col items-center gap-8 pt-32">
+        <div className="page-layout">
             <h1 className="p-1 text-2xl underline">Pagination Experiments</h1>
             <div className="border-foreground rounded-md border p-2">
                 <NavigationMenu>

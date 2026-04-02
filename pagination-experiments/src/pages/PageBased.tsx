eaa9e5d8863e4a9273badbbc40f2c532e3e6f099
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-02 13:47:04 +0300

    Handle pending and error while fetching products.

--- pagination-experiments/src/pages/PageBased.tsx
+++ pagination-experiments/src/pages/PageBased.tsx
@@ -9,7 +9,6 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination";
-import { SAMPLE_PRODUCT } from "@/schemas/product.schema";
 import { useQuery } from "@tanstack/react-query";
 
 export default function PageBased() {
@@ -25,17 +24,21 @@ export default function PageBased() {
         staleTime: 1000 * 60,
     });
 
-    if (isPending) return <div>Pending placeholder...</div>;
-    if (error) return <div>Error placeholder...</div>;
-
     return (
         <div className="page-layout">
             <h1 className="p-1 text-2xl underline">Page-based pagination</h1>
-            <div className="grid grid-cols-2 gap-8">
-                {products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
+            {isPending ? (
+                <div>Fetching products...</div>
+            ) : error ? (
+                <div>Error while fetching products: {error.message}</div>
+            ) : (
+                <div className="grid grid-cols-2 gap-8">
+                    {products.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+            )}
+
             <Pagination>
                 <PaginationContent>
                     <PaginationItem>

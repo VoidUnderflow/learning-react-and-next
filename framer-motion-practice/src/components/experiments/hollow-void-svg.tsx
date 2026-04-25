4a1af0009c3606b8803538b6d2dfe673f5db7179
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-25 13:30:21 +0300

    Add notes on some of the attributes.

--- framer-motion-practice/src/components/experiments/hollow-void-svg.tsx
+++ framer-motion-practice/src/components/experiments/hollow-void-svg.tsx
@@ -4,8 +4,12 @@ export default function HollowVoidSvg() {
   return (
     <div className="relative flex h-32 w-full flex-col items-center">
       <svg
+        // width, height = just like a div
         width={600}
         height={200}
+        // SVG's internal coord system: min-x min-y width height
+        // min_x min_y -> (0, 0) = origin, top left
+        // Oy points downwards
         viewBox="0 0 600 200"
         className="absolute top-0"
       >
@@ -14,8 +18,16 @@ export default function HollowVoidSvg() {
           fontSize="128px"
           stroke="white"
           strokeWidth={1}
+          // Horizontal position of the text anchor point.
+          // 50% -> halfway across the viewBox width, but start point may differ.
           x="50%"
+          // Vertical position of the text anchor point.
+          // y = text bottom
           y="70%"
+          // which part of the text does x refer to?
+          // start => x == left edge of the text
+          // middle => x == horizontal center of the text
+          // end => x == right edge
           textAnchor="middle"
           animate={{ fontSize: [128, 131, 128] }}
           transition={{

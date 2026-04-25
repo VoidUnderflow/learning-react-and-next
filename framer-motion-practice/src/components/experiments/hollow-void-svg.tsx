a80a666ae95ba139dab279b4a35f18c03f58fc58
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-25 13:19:28 +0300

    Fix layout shift.

--- framer-motion-practice/src/components/experiments/hollow-void-svg.tsx
+++ framer-motion-practice/src/components/experiments/hollow-void-svg.tsx
@@ -2,26 +2,33 @@ import { motion } from "motion/react";
 
 export default function HollowVoidSvg() {
   return (
-    <svg width={600} height={200} viewBox="0 0 600 200">
-      <motion.text
-        fontWeight="bold"
-        fontSize="128px"
-        stroke="white"
-        strokeWidth={1}
-        x="50%"
-        y="70%"
-        textAnchor="middle"
-        animate={{ fontSize: [128, 131, 128] }}
-        transition={{
-          duration: 1,
-          ease: "easeInOut",
-          repeat: Infinity,
-          repeatDelay: 2,
-          times: [0, 0.1, 1],
-        }}
+    <div className="relative flex h-32 w-full flex-col items-center">
+      <svg
+        width={600}
+        height={200}
+        viewBox="0 0 600 200"
+        className="absolute top-0"
       >
-        VOID
-      </motion.text>
-    </svg>
+        <motion.text
+          fontWeight="bold"
+          fontSize="128px"
+          stroke="white"
+          strokeWidth={1}
+          x="50%"
+          y="70%"
+          textAnchor="middle"
+          animate={{ fontSize: [128, 131, 128] }}
+          transition={{
+            duration: 1,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatDelay: 2,
+            times: [0, 0.1, 1],
+          }}
+        >
+          VOID
+        </motion.text>
+      </svg>
+    </div>
   );
 }
--- framer-motion-practice/src/components/experiments/hollow-void.tsx
+++ framer-motion-practice/src/components/experiments/hollow-void.tsx
@@ -2,18 +2,20 @@ import { motion } from "motion/react";
 
 export default function HollowVoid() {
   return (
-    <motion.h1
-      className="text-9xl font-bold text-transparent [-webkit-text-stroke:1px_white]"
-      animate={{ fontSize: ["8rem", "8.2rem", "8rem"] }}
-      transition={{
-        duration: 1,
-        ease: "easeInOut",
-        repeat: Infinity,
-        repeatDelay: 2,
-        times: [0, 0.1, 1],
-      }}
-    >
-      VOID
-    </motion.h1>
+    <div className="relative flex h-32 w-full flex-col items-center">
+      <motion.h1
+        className="absolute top-0 mx-auto text-9xl font-bold text-transparent [-webkit-text-stroke:1px_white]"
+        animate={{ fontSize: ["8rem", "8.2rem", "8rem"] }}
+        transition={{
+          duration: 1,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 2,
+          times: [0, 0.1, 1],
+        }}
+      >
+        VOID
+      </motion.h1>
+    </div>
   );
 }

730aba671cad34bb8fa692e01c67ec0d3894a9cd
Mihai Ciobanu <alexmihai.ciobanu@gmail.com>
2026-04-25 13:15:00 +0300

    Add pulsating text as svg.

---
+++ framer-motion-practice/src/components/experiments/hollow-void-svg.tsx
@@ -0,0 +1,27 @@
+import { motion } from "motion/react";
+
+export default function HollowVoidSvg() {
+  return (
+    <svg width={600} height={200} viewBox="0 0 600 200">
+      <motion.text
+        fontWeight="bold"
+        fontSize="128px"
+        stroke="white"
+        strokeWidth={1}
+        x="50%"
+        y="70%"
+        textAnchor="middle"
+        animate={{ fontSize: [128, 131, 128] }}
+        transition={{
+          duration: 1,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 2,
+          times: [0, 0.1, 1],
+        }}
+      >
+        VOID
+      </motion.text>
+    </svg>
+  );
+}
--- framer-motion-practice/src/pages/Experiments.tsx
+++ framer-motion-practice/src/pages/Experiments.tsx
@@ -1,10 +1,14 @@
 import HollowVoid from "../components/experiments/hollow-void";
+import HollowVoidSvg from "../components/experiments/hollow-void-svg";
 
 export default function Experiments() {
   return (
     <div className="page-layout">
       <h2>Experiments</h2>
+      <p>Pulsating void</p>
       <HollowVoid />
+      <p>Pulsating void as an svg</p>
+      <HollowVoidSvg />
     </div>
   );
 }
